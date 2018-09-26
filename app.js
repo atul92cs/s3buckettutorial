fb236d7a3d30d2acbf44e5b95e56c575d7868e06
atul92cs <atul.11192@gmail.com>
2018-09-26 20:09:13 +0530

    updating the code for a work

---
+++ app.js
@@ -0,0 +1,44 @@
+var aws =require('aws-sdk');
+var express=require('express');
+var multer=require('multer');
+var multerS3=require('multer-s3');
+var bodyParser=require('body-parser');
+var aws=require('aws-sdk');
+var app=express();
+
+aws.config.update({
+  secretAccessKey:'4Kmh6CIT3O6klSLkGf0ULl9UwrPT9Agjpd1qexxv',
+  accessKeyId:'AKIAJ5S6DR6RYOY6YO6A',
+  region:'us-east-1'
+});
+
+var app=express(),
+    s3=new aws.S3();
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.set('views',__dirname+'/views');
+
+app.set('view engine','ejs')
+
+var upload=multer({
+    storage:multerS3({
+	 s3:s3,
+	 bucket:'s3trialfirstnode',
+	 key:function(req,file,cb){
+	  console.log(file);
+	  cb(null,file.originalname);
+	 }
+	})
+});
+
+app.get('/',function(req,res){
+   res.render('index.ejs');
+});
+
+app.post('/upload',upload.array('upl',1),function(req,res,next){
+   res.send('fille uploaded');
+});
+
+app.listen(3000,function(){
+   console.log('app started');
+});
